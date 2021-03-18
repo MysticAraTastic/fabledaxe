3c11e8f4dca8e606994882b6fd9c7f8a88793bdd
MysticAraTastic <dreamsuperbig.123@gmail.com>
2021-03-18 16:01:56 -0400

    Added porcesses

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ytdl = require('ytdl-core');
 const { YTSearcher } = require('ytsearcher');
  
 const searcher = new YTSearcher({
-    key: process.env.youtube_api,
+    key: process.env.token,
     revealed: true
 });
  
